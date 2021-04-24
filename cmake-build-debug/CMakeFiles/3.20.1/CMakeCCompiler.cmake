b9c1e969493524617b0c2d893a38e2ead8c71375
swagger <zsw18250205073@gmail.com>
2021-04-24 21:16:15 +0800

    update

--- cmake-build-debug/CMakeFiles/3.20.1/CMakeCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.20.1/CMakeCCompiler.cmake
@@ -17,6 +17,7 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
+
 set(CMAKE_AR "/Library/Developer/CommandLineTools/usr/bin/ar")
 set(CMAKE_C_COMPILER_AR "")
 set(CMAKE_RANLIB "/Library/Developer/CommandLineTools/usr/bin/ranlib")
@@ -47,6 +48,7 @@ set(CMAKE_C_LINKER_PREFERENCE 10)
 # Save compiler ABI information.
 set(CMAKE_C_SIZEOF_DATA_PTR "8")
 set(CMAKE_C_COMPILER_ABI "")
+set(CMAKE_C_BYTE_ORDER "LITTLE_ENDIAN")
 set(CMAKE_C_LIBRARY_ARCHITECTURE "")
 
 if(CMAKE_C_SIZEOF_DATA_PTR)
